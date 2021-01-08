a41b12666654972f063e769fe54100ede70483f5
bittergourdking <>
2021-01-08 00:29:55 -0500

    ran through basictester, seems to be good!

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -13,9 +13,16 @@ public class MyLinkedList {
 
  public boolean add(String value) {
    Node newNode = new Node(value);
-   end.setNext(newNode);
-   newNode.setPrev(end);
-   end = newNode;
+   if (size == 0) {
+     start = end = newNode;
+     start.setPrev(null);
+     end.setNext(null);
+   } else {
+     end.setNext(newNode);
+     newNode.setPrev(end);
+     newNode.setNext(null);
+     end = newNode;
+   }
    size++;
    return true;
  }
@@ -29,34 +36,66 @@ public class MyLinkedList {
  }
 
  public void add(int index, String value) {
+   if (index < 0 || index > size) {
+     throw new IndexOutOfBoundsException("Invalid index!");
+   }
    Node newNode = new Node(value);
-   if (index != 0) {
-     findIndex(index - 1).setNext(newNode);
-     newNode.setPrev(findIndex(index - 1));
-   }
-   if (index != size) {
-     findIndex(index).setPrev(newNode);
+   if (size == 0) {
+     start = end = newNode;
+     start.setPrev(null);
+     end.setNext(null);
+   } else if (index == 0) {
+     newNode.setNext(start);
+     newNode.setPrev(null);
+     start.setPrev(newNode);
+     start = newNode;
+   } else if (index == size) {
+     newNode.setPrev(start);
+     newNode.setNext(null);
+     end.setNext(newNode);
+     end = newNode;
+   } else {
      newNode.setNext(findIndex(index));
+     newNode.setPrev(findIndex(index).getPrev());
+     findIndex(index - 1).setNext(newNode);
+     findIndex(index).setPrev(newNode);
    }
+   size++;
  }
 
  public String get(int index) {
+   if (index < 0 || index > size) {
+     throw new IndexOutOfBoundsException("Invalid index!");
+   }
    return findIndex(index).getData();
  }
 
  public String set(int index, String value) {
+   if (index < 0 || index > size) {
+     throw new IndexOutOfBoundsException("Invalid index!");
+   }
    return findIndex(index).setData(value);
  }
 
  public String toString() {
-   //fix
    Node current = start;
    String answer = "[";
    while (current != null) {
      answer += current.getData() + ", ";
      current = current.getNext();
     }
-   return answer.substring(0, answer.length() - 2);
+   return answer.substring(0, answer.length() - 2) + "]";
  }
+
+ public String toStringReversed() {
+   Node current = end;
+   String answer = "[";
+   while (current != null) {
+     answer += current.getData() + ", ";
+     current = current.getPrev();
+    }
+   return answer.substring(0, answer.length() - 2) + "]";
+ }
+
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
